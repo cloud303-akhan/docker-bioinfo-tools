f841fc9905de4bf02cb8b0292c5f6f4b811fb632
Elaine Gee <elaine@mirvie.com>
2021-09-14 16:00:42 -0700

    Add force removal since images depend on mirbase and blocks its removal

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ do
 	exist=`docker image inspect $image:latest >/dev/null 2>&1 && echo yes || echo no`
 	if [ "$exist" = "yes" ]; then
 		echo "Removing previous local image"
-		docker image rm $image
+		docker image rm -f $image
 	fi
 	docker build -t $image:latest - < $image/Dockerfile
 done
