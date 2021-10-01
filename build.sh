84eb3879f8496f347db2d0a2bcbc7fb77f6eeb4a
Elaine Gee <elaine@mirvie.com>
2021-09-30 17:02:58 -0700

    Run images as dockworker

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 set -e  # fail on any error
 
-for image in mirbase mirbenedict mirpandas mirfastqc mirhtseq mirpicard mirrseqc mirsamtools mirstar mirtrimmomatic
+for image in mirbase mirbenedict mirpandas mirfastqc mirhtseq mirpicard mirrseqc mirsamtools mirstar mirtrimmomatic mirmultiqc
 do
-	echo "> Creating image $image"
+	echo ""
+	echo ">> Creating image $image"
 	exist=`docker image inspect $image:latest >/dev/null 2>&1 && echo yes || echo no`
 	if [ "$exist" = "yes" ]; then
 		echo "Removing previous local image"
 		docker image rm -f $image
 	fi
 	docker build -t $image:latest - < $image/Dockerfile
+        echo "***--> COMPLETE!"
 done
