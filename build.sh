4b5b96fef5d860c491fa665e8ae770e29667c44a
cloud303-xtoro <xtoro@cloud303.io>
2021-12-14 10:02:43 -0500

    Push with Commit hash

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
+cd mirbase
+docker build -t mirbase -f Dockerfile .
+cd ..
+
 PROJECTS=($(ls -d */ | tr -d /))
+COMMIT_HASH=$(git rev-parse --short HEAD)
 
 for project in "${PROJECTS[@]}";
     do
     cd $project/
     docker build -t $project -f Dockerfile .
-    docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$CODEBUILD_BUILD_NUMBER"
+    docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
-    docker push "${ECR_REPOSITORY_URI}/${project}:release-$CODEBUILD_BUILD_NUMBER"
+    docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker push "${ECR_REPOSITORY_URI}/${project}:latest"
 
     cd ..
