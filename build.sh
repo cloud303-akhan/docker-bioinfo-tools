3328392f1fe3a680c94951d2d699c8e18769d207
cloud303-xtoro <xtoro@cloud303.io>
2021-12-21 16:24:14 -0500

    ignore bclconvert image

--- build.sh
+++ build.sh
@@ -12,12 +12,19 @@ cd ..
 
 for project in "${PROJECTS[@]}";
     do
+	if [[ "$project" == "mirbclconvert" ]]
+	then
+		continue
+	fi
+	echo "############ Bulding $project #############"
     cd $project/
+
     docker build -t $project --build-arg GIT_COMMIT=$COMMIT_HASH -f Dockerfile .
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
     docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker push "${ECR_REPOSITORY_URI}/${project}:latest"
+	echo "############ Done $project #############"
 
     cd ..
 
