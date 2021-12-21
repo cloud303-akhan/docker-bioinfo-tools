f652b211f1fa446cd86d7031b44fc4f6d6323d00
cloud303-xtoro <xtoro@cloud303.io>
2021-12-21 16:05:36 -0500

    Add Commit_hash on image tag

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 
 set -e
 
-cd mirbase
-docker build -t mirbase -f Dockerfile .
-cd ..
-
 PROJECTS=($(ls -d */ | tr -d /))
 COMMIT_HASH=$(git rev-parse --short HEAD)
+
+cd mirbase
+docker build -t mirbase:$COMMIT_HASH -f Dockerfile .
+cd ..
+
 
 for project in "${PROJECTS[@]}";
     do
     cd $project/
-    docker build -t $project -f Dockerfile .
+    docker build -t $project --build-arg GIT_COMMIT=$COMMIT_HASH -f Dockerfile .
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
     docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
