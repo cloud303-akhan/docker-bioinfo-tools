b5632fc9f28977f1421468eda488c4a997551725
Ahmed Alam Khan <akhan@cloud303.io>
2023-02-21 16:01:47 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,7 @@
 ECR_REPOSITORY_URI=570351108046.dkr.ecr.us-east-1.amazonaws.com
 PROJECTS=($(ls -d */ | tr -d /))
 COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | head -c 7)
-
-###
-echo COMMIT_HASH=$COMMIT_HASH
-echo CODEBUILD_SOURCE_VERSION=$CODEBUILD_SOURCE_VERSION
-echo CODEBUILD_WEBHOOK_PREV_COMMIT=$CODEBUILD_WEBHOOK_PREV_COMMIT
-echo CODEBUILD_WEBHOOK_HEAD_REF=$CODEBUILD_WEBHOOK_HEAD_REF
-echo CODEBUILD_SOURCE_REPO_URL=$CODEBUILD_SOURCE_REPO_URL
-###
+# COMMIT_HASH=$(git rev-parse --short HEAD)
 
 cd mirbase
 docker build -t mirbase:$COMMIT_HASH -f Dockerfile .
@@ -20,9 +13,27 @@ cd ..
 
 for project in "${PROJECTS[@]}";
     do
-	if [[ "$project" == "mirbclconvert" ]]
+	if [[ "$project" == "mirbase" ]]
 	then
 		continue
+	elif [[ "$project" == "mirbclconvert" ]]
+	then
+		continue
+	elif [[ "$project" == "mircheckfastq" ]]
+	then
+        echo "############ Bulding $project #############"
+        cd $project/
+
+        docker build -t $project --build-arg GIT_COMMIT=$COMMIT_HASH -f Dockerfile .
+        docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
+        docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
+        docker run "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH" biopet-validatefastq --version
+        docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
+        docker push "${ECR_REPOSITORY_URI}/${project}:latest"
+        echo "############ Done $project #############"
+
+        cd ..
+        continue
 	fi
 	echo "############ Bulding $project #############"
     cd $project/
@@ -30,6 +41,7 @@ for project in "${PROJECTS[@]}";
     docker build -t $project --build-arg GIT_COMMIT=$COMMIT_HASH -f Dockerfile .
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
+    docker run "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH" "${project:3}" --version
     docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
     docker push "${ECR_REPOSITORY_URI}/${project}:latest"
 	echo "############ Done $project #############"
