c0eb1e0544d53056e332ece939095a62048b975f
Ahmed Alam Khan <akhan@cloud303.io>
2023-02-21 11:29:10 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-echo "Testing on Nov 9"
-echo $1
-
 # set -e
-ECR_REPOSITORY_URI=$1.dkr.ecr.us-east-1.amazonaws.com
+ECR_REPOSITORY_URI=570351108046.dkr.ecr.us-east-1.amazonaws.com
 PROJECTS=($(ls -d */ | tr -d /))
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-###  Delete this later
-echo $ECR_REPOSITORY_URI
-echo $PROJECTS
+###
 echo $COMMIT_HASH
 ###
 
 cd mirbase
-docker build -t mirbase:$COMMIT_HASH -f Dockerfile --build-arg AWS_ACCOUNT_ID=$1 .
+docker build -t mirbase:$COMMIT_HASH -f Dockerfile .
 cd ..
 
-for project in "${PROJECTS[@]}"; do
-	if [ "$project" != "tests" ]; then
-		echo "############ Bulding $project #############"
-        cd $project/
-        docker build -t $project --build-arg GIT_COMMIT=$COMMIT_HASH --build-arg AWS_ACCOUNT_ID=$1 -f Dockerfile .
-        docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
-        docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
-        docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
-        docker push "${ECR_REPOSITORY_URI}/${project}:latest"
-        echo "############ Done $project #############"
-        cd ..
+
+for project in "${PROJECTS[@]}";
+    do
+	if [[ "$project" == "mirbclconvert" ]]
+	then
+		continue
 	fi
+	echo "############ Bulding $project #############"
+    cd $project/
+
+    docker build -t $project --build-arg GIT_COMMIT=$COMMIT_HASH -f Dockerfile .
+    docker tag $project "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
+    docker tag $project "${ECR_REPOSITORY_URI}/${project}:latest"
+    docker push "${ECR_REPOSITORY_URI}/${project}:release-$COMMIT_HASH"
+    docker push "${ECR_REPOSITORY_URI}/${project}:latest"
+	echo "############ Done $project #############"
+
+    cd ..
+
 done
 
 aws ssm put-parameter --overwrite --name /VERSION/DOCKER_RUNTIME --value release-$COMMIT_HASH
